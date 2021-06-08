aed30f1b7957f7a2e1f151a61004a2fe80e55ac8
programacionlmor <84878779+programacionlmor@users.noreply.github.com>
2021-06-08 12:39:17 -0500

    Update Cadena_Caracteres.py

--- Cadena_Caracteres.py
+++ Cadena_Caracteres.py
@@ -65,7 +65,7 @@
 # # Porciones o rebanadas o slice de un string 
 # #######################################################################
 
-# nombreFruta = 'naranja'
+#nombreFruta = 'naranja'
 
 # tresLetras     = nombreFruta[0:3]
 # print(tresLetras)
@@ -78,6 +78,12 @@
 
 # porcionInicial= nombreFruta[:2]
 # print(porcionInicial)
+
+# porcion = nombreFruta[-1:-3]  # No funciona
+# print(porcion)
+
+# porcion = nombreFruta[-3:-1]  # No funciona
+# print(porcion)
 
 # ####################################################################################################
 # #Si elprimer indice es  mayor al seguno  o los indices son iguales el resultado es una cadena vacia
@@ -118,6 +124,12 @@
 
 # nombreFruta= "naranja"
 # if not ( "ran" in nombreFruta):
+#    print ("No imprima nada")
+# else:
+#    print ("naranja incluye los caracteres ran")
+
+# nombreFruta= "naranja"
+# if  "ran" not in nombreFruta:
 #    print ("No imprima nada")
 # else:
 #    print ("naranja incluye los caracteres ran")
@@ -167,7 +179,6 @@
 # if cadena.isalnum():
 #    print("es cadena alfanumérica") 
 
-
 ####################################
 # Dividir una cadena por un caracter
 ####################################
@@ -175,7 +186,6 @@
 # nombre, dominio = correo.split("@")
 # print(nombre)
 # print(dominio)
-
 
 
 
@@ -224,3 +234,127 @@
 # nombreFruta = "sandia"
 # calorias = 20.88
 # print(f' La {nombreFruta} es una fruta tropical y tiene {calorias} calorias ')
+
+
+#############################################
+# Ciclo repetitivo con cadena de caracteres
+#############################################
+
+# nombreFruta = "sandia"
+
+# for caracterFruta in nombreFruta:
+#   print(caracterFruta,end=" ")
+
+
+
+#############################################
+# Otros Metodos con cadena de caracteres
+############################################
+
+######################################################################
+#capitalize() - Convierte la primera letra de la cadena en mayusculas
+######################################################################
+# nombreFruta = "sandia"
+
+# mayuscula = nombreFruta.capitalize()
+# print(mayuscula)
+
+
+####################################################################
+# title() - Convierte la primera letra de cada palabra en  mayúscula
+###################################################################
+# nombreFruta = " sandia , pera, mora"
+# mayuscula = nombreFruta.title()
+# print(mayuscula)
+
+
+####################################################################
+# swapcase() - Convierte mayúsculas en minúsculas y viceversa
+###################################################################
+
+# nombreFruta = " Sandia , Pera, Mora"
+# mayuscula = nombreFruta.swapcase()
+# print(mayuscula)
+
+
+
+#####################################################################
+# casefold - Retorna un string con todos los caracteres en minúscula
+#####################################################################
+
+# nombreFruta = " Sandia , Pera, Mora"
+# minuscula = nombreFruta.casefold()
+# print(minuscula)
+
+#####################################################################
+# find - Encuentra la ocurrencia de un valor especificado
+#        Retorna -1 si el valor no fue encontrado
+#        Retorna la posición si fue encontrado
+#####################################################################
+
+# nombreFruta = " Sandia , Pera, Mora"
+# posicion = nombreFruta.find("Pera")
+# print(posicion)
+
+
+# nombreFruta = " Sandia , Pera, Mora"
+# posicion = nombreFruta.find("Pera",11,17) # Busca en el rango de posiciones.
+# print(posicion)
+
+
+
+########################################################################
+# index  - Encuentra la ocurrencia de un valor especificado
+#          Retorna la posición si fue encontrado. Sino lanza excepción
+########################################################################
+
+# nombreFruta = " Sandia , Pera, Mora"
+# posicion = nombreFruta.index("Pera")
+# print(posicion)
+
+
+# nombreFruta = " Sandia , Pera, Mora"
+# posicion = nombreFruta.index("Pera",10,15) # Busca en el rango de posiciones.
+# print(posicion)
+
+
+# nombreFruta = " Sandia , Pera, Mora"
+# posicion = nombreFruta.index("Pera",20) # Busca apartir de la posicion. Marca error
+# print(posicion)
+
+
+
+
+################################################
+# replace() - Remplaza unos caracteres por otros
+################################################
+# nombreFruta = " Sandia , Pera, Mora"
+# cambio = nombreFruta.replace("Pera", "mango") #remplaza Pera por mango
+# print(cambio)
+
+
+# nombreFruta = " Sandia , Pera, Mora"
+# cambio = nombreFruta.replace("pera", "mango") #Si no encuentra devuelve la misma cadena.
+# print(cambio)
+
+# nombreFruta = " Sandia , Pera, Mora , Pera, Pera"
+# cambio  = nombreFruta.replace("Pera", "mango", 2) # Remplaza las dos primeras ocurrencias
+# print(cambio)
+
+
+######################################################################
+# strip() - Remueve los espacios al principio y al final de la cadena
+######################################################################
+
+# nombreFruta = "  Sandia , Pera, Mora , Pera, Pera  "
+# sinEspacios = nombreFruta.strip()
+# print(sinEspacios)
+
+######################################################################
+# strip() - Remueve los caracteres especificados
+######################################################################
+
+# nombreFruta = ",,,,,rrttggmmmm.....banana....rrr"
+# sinCaracteres = nombreFruta.strip(",.grtm") # remueve los caracteres indicados.
+# print(sinCaracteres)
+
