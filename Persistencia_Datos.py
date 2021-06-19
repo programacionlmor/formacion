8a135ffc9b4f4d53df96cd74bf0a1807dfdf7cb9
programacionlmor <84878779+programacionlmor@users.noreply.github.com>
2021-06-19 10:16:13 -0500

    Update Persistencia_Datos.py

--- Persistencia_Datos.py
+++ Persistencia_Datos.py
@@ -12,30 +12,78 @@
 ####################################################################################################
 # Leer un archivo de texto en python # 
 # "r" - Read -  Valor por defecto. Abre el archivo para lectura. Saca error si el archivo no existe
-# "x" - Create - Creará un archivo retornará un error si el archivo existe
-# "a" - Append - Creará una arhivo si el archivo especificado no existe
-# "w" - Write -  Creará un archivo si el archivo especificado no existe
 #####################################################################################################
 
 # try:
-#     archivo = open("archivo_prueba.txt")  # Abre el archivo
-#     #print(archivo.read())      # Lee todo el contenido del archivo.
-#     #print(archivo.read(5))     # Lee cinco caracteres del archivo.
-#     #print(archivo.readline())  # Lee la primera línea del archivo.
-#     #print(archivo.readline())  # Lee la segunda línea del archivo.
+#   archivo = open("archivo_prueba.txt")  # Abre el archivo
+    #print(archivo.read())      # Lee todo el contenido del archivo.
+    #print(archivo.read(5))     # Lee cinco caracteres del archivo.
+    # print(archivo.readline())  # Lee la primera línea del archivo.
+    # print(archivo.readline())  # Lee la segunda línea del archivo.
 
-#     for linea in archivo:       # Recorrer linea a linea el archivo por un for
-#         print(linea,end="") 
-#     archivo.close()             # Cerrar el archivo
-#     archivo.readline()          # Genera excepción al no encontrarse el archivo abierto
+    # for linea in archivo:       # Recorrer linea a linea el archivo por un for
+    #     print(linea,end="") 
+    # archivo.close()             # Cerrar el archivo
+    # archivo.readline()          # Genera excepción al no encontrarse el archivo abierto
 
 # except:
 #     print("\n Error procesando el archivo")
 
 
-######################################
-# Escribir en un archivo existente   # 
-######################################
+#################################################
+# Guardar en una lista las líneas de un archivo
+#################################################
+
+# listaLineas = []
+# with open("archivo_prueba.txt") as archivo:
+# 	lineas = archivo.readlines()
+# 	for linea in lineas:
+# 		listaLineas.append(linea.strip('\n'))
+# print (listaLineas)
+
+
+#############################################################
+# Guardar en listas las palabras de cada linea de un archivo
+#############################################################
+
+# listaPalabras = []
+# with open("archivo_prueba.txt") as archivo:
+# 	for lineas in archivo:
+# 		listaPalabras.append(lineas.split())
+
+# print (listaPalabras)
+
+
+#############################################################
+# Guardar en un sola lista las lineas  de un archivo
+#############################################################
+
+# listaPalabras = []
+# with open("archivo_prueba.txt") as archivo:
+# 	for lineas in archivo:
+# 		listaPalabras.append(lineas.strip("\n"))
+
+# print (listaPalabras)
+
+#############################################################
+# Guardar en un sola lista las palabras de un archivo
+#############################################################
+
+# listaPalabras = []
+# with open("archivo_prueba.txt") as archivo:
+# 	for lineas in archivo:
+# 		 listaPalabras.extend(lineas.strip("\n"))
+
+# print (listaPalabras)
+
+
+
+#############################################################################
+# Escribir en un archivo existente                                          # 
+# "a" - Append - Creará una arhivo si el archivo especificado no existe     #
+# "x" - Create - Creará un archivo retornará un error si el archivo existe  #
+# "w" - Write -  Creará un archivo si el archivo especificado no existe     #
+#############################################################################
 
 #try:
     # archivo = open("archivo_prueba.txt","a")             # Abre el archivo en modo append. Añade al final del archivo
@@ -47,11 +95,11 @@
 
     # ------------------------------------------
     
-    # archivo = open("archivo_prueba.txt","w") 
-    # archivo.write("Vamos a sobreescribir todo el archivo")  #  Sobreescribir el archivo.
-    # archivo.close()                                      # Cerrar el archivo
-    # archivo = open("archivo_prueba.txt","r")             # Abre el archivo en modo lectura.
-    # print(archivo.read() )
+#     archivo = open("archivo_prueba.txt","w") 
+#     archivo.write("Vamos a sobreescribir todo el archivo")  #  Sobreescribir el archivo.
+#     archivo.close()                                         # Cerrar el archivo
+#     archivo = open("archivo_prueba.txt","r")                # Abre el archivo en modo lectura.
+#     print(archivo.read() )
 
 
 # except:
@@ -60,7 +108,6 @@
 
 #########################################################################
 # Crear un archivo 
-# 
 # "x" - Create - Creará un archivo retornará un error si el archivo existe
 # "a" - Append - Creará una arhivo si el archivo especificado no existe
 # "w" - Write -  Creará un archivo si el archivo especificado no existe
@@ -70,7 +117,7 @@
 # Caso 1
 ##########
 # try:
-#     archivo = open("archivo_prueba.txt","x") # Genera excepción porque el archivo ya existe
+#     archivo = open("archivo_prueba_cinco.txt","x")  # Genera excepción porque el archivo ya existe
 # except:
 #     print("Error procesando el archivo")
 
@@ -79,8 +126,8 @@
 # Caso 2
 ##########
 # try:
-#     archivo = open("archivo_prueba.txt","w") # El archivo existe pero no genera excepción
-#     archivoDos = open("archivo_prueba_dos.txt","w") #El archivo no existe y lo crea
+#     archivo    = open("archivo_prueba.txt","w") # El archivo existe pero no genera excepción
+#     archivoDos = open("archivo_prueba_seis.txt","w") #El archivo no existe y lo crea
 # except:
 #     print("Error procesando el archivo")
 
@@ -89,8 +136,8 @@
 # Caso 3
 ##########
 # try:
-#     archivo = open("archivo_prueba.txt","a") # El archivo existe pero no genera excepción
-#     archivoTres = open("archivo_prueba_tres.txt","a") # El archivo no existe y lo crea
+#     archivo     = open("archivo_prueba.txt","a") # El archivo existe pero no genera excepción
+#     archivoTres = open("archivo_prueba_ocho.txt","a") # El archivo no existe y lo crea
 # except:
 #     print("Error procesando el archivo")
 
@@ -116,7 +163,7 @@
 ##########
 
 # import os 
-# if os.path.exists("archivo_prueba.txt"):
+# if os.path.exists("archivo_prueba.txt"):  # Valida primero si el archivo existe
 #    os.remove("archivo_prueba.txt")
 # else:
 #   print("El archivo no existe")
